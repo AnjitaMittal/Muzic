abed07ccd85bdd995e45775e72b6d1b09218146d
Varun Dixit <wookupmaker@gmail.com>
2018-09-04 18:03:57 +0530

    Bit 4

--- app/src/main/java/science/wookup/muzic/MainActivity.java
+++ app/src/main/java/science/wookup/muzic/MainActivity.java
@@ -1,5 +1,6 @@
 package science.wookup.muzic;
 
+import android.content.Intent;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,15 +12,12 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+    static final String KEY_SONG = "key_song";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        final DownloadThread thread = new DownloadThread();
-        thread.setName("Downloading Thread");
-        thread.start();
 
         Button downloadSongButton = findViewById(R.id.downloadSong);
         downloadSongButton.setOnClickListener(new View.OnClickListener() {
@@ -29,11 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
                 // Send messages or runnable to handler
                 for (String song: Songs.songs) {
-                    Message message = Message.obtain();
-                    message.obj = song;
-                    thread.downloadHandler.sendMessage(message);
+                    Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                    intent.putExtra(KEY_SONG, song);
+                    startService(intent);
                 }
-
             }
         });
     }
