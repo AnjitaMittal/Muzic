6f7a0b62825a6608c5607894f7876f2460705f69
Varun Dixit <wookupmaker@gmail.com>
2018-09-04 17:49:52 +0530

    Bit 3

--- app/src/main/java/science/wookup/muzic/DownloadThread.java
+++ app/src/main/java/science/wookup/muzic/DownloadThread.java
@@ -1,26 +1,16 @@
 package science.wookup.muzic;
 
+import android.os.Looper;
 import android.util.Log;
 
 public class DownloadThread extends Thread {
     private static final String TAG = DownloadThread.class.getSimpleName();
+    public DownloadHandler downloadHandler;
 
     @Override
     public void run() {
-        for (String song: Songs.songs) {
-            downloadSong();
-        }
-    }
-
-    private void downloadSong() {
-        long endTime = System.currentTimeMillis() + 10*1000;
-        while(System.currentTimeMillis() < endTime) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Log.d(TAG, "Song Downloaded!");
+        Looper.prepare();
+        downloadHandler = new DownloadHandler();
+        Looper.loop();
     }
 }
--- app/src/main/java/science/wookup/muzic/MainActivity.java
+++ app/src/main/java/science/wookup/muzic/MainActivity.java
@@ -1,5 +1,6 @@
 package science.wookup.muzic;
 
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,14 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final DownloadThread thread = new DownloadThread();
+        thread.setName("Downloading Thread");
+        thread.start();
+
         Button downloadSongButton = findViewById(R.id.downloadSong);
         downloadSongButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Downloading...", Toast.LENGTH_SHORT).show();
-                Thread thread = new DownloadThread();
-                thread.setName("Downloading Thread");
-                thread.start();
+
+                // Send messages or runnable to handler
+                for (String song: Songs.songs) {
+                    Message message = Message.obtain();
+                    message.obj = song;
+                    thread.downloadHandler.sendMessage(message);
+                }
 
             }
         });
