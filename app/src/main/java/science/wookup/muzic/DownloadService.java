a5457e927d81ac75b9326f70a82178130f2f7252
Varun Dixit <wookupmaker@gmail.com>
2018-09-04 19:20:37 +0530

    Bit 5

--- app/src/main/java/science/wookup/muzic/DownloadService.java
+++ app/src/main/java/science/wookup/muzic/DownloadService.java
@@ -3,32 +3,34 @@ package science.wookup.muzic;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
 import static science.wookup.muzic.MainActivity.KEY_SONG;
 
 public class DownloadService extends Service {
-
     private static final String TAG = DownloadService.class.getSimpleName();
+    private DownloadHandler downloadHandler;
+
+    @Override
+    public void onCreate() {
+        DownloadThread downloadThread = new DownloadThread();
+        downloadThread.setName("Download Thread");
+        downloadThread.start();
+
+        while (downloadThread.downloadHandler == null) {}
+
+        downloadHandler = downloadThread.downloadHandler;
+    }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         String song = intent.getStringExtra(KEY_SONG);
-        downloadSong(song);
+        Message message = Message.obtain();
+        message.obj = song;
+        downloadHandler.handleMessage(message);
         return Service.START_REDELIVER_INTENT;
-    }
-
-    private void downloadSong(String song) {
-        long endTime = System.currentTimeMillis() + 10*1000;
-        while(System.currentTimeMillis() < endTime) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        Log.d(TAG, song + " Downloaded!");
     }
 
     @Nullable
